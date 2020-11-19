e20d3b1a65eaca4087bd41848f2d5cbe2827cade
Tomer Gibor <tomergibor@gmail.com>
2020-11-19 19:56:22 +0200

    Added app

--- Server/server.py
+++ Server/server.py
@@ -1,5 +1,6 @@
 import base64
 import io
+from datetime import datetime
 from typing import Dict
 
 import numpy as np
@@ -18,21 +19,23 @@ class Data(BaseModel):
     b64image: str
     preprocessing: bool
 
+
 i = 0
 
+
 @app.post('/convert')
-async def predict_text(data: Data) -> Dict[str, str]:
+async def image_to_text(data: Data) -> Dict[str, str]:
     global i
     base64_decoded = base64.b64decode(data.b64image)
     pil_image = PIL.Image.open(io.BytesIO(base64_decoded)).convert('L')
-    pil_image.save('17-11-2020.png')
+    pil_image.save(f'{datetime.now()}.png')
     print(data.preprocessing)
 
     # print(image.b64image)
     i += 1
     return {'result': f'test{i}'}
-    # pil_image = PIL.Image.open(io.BytesIO(base64_decoded)).convert('L')
-    #
+
+
     # np_image = np.asarray(pil_image)
     #
     # text = text_from_image(np_image)
