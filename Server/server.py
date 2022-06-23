7680fe8019c6dee44e36d04b232ea4ed7e79a059
Tomer Gibor <tomergibor@gmail.com>
2022-06-23 14:12:25 +0300

    Upgraded Tensoflow-gpu to version 2.9.1

--- Server/server.py
+++ Server/server.py
@@ -59,6 +59,16 @@ async def invalid_b64_img_handler(request: Request,
     )
 
 
+
+
+
+
+
+
+
+
+
+
 def decode_image(b64image: str) -> np.ndarray:
     """Try decoding the image from base64 string, into numpy array."""
     try:
